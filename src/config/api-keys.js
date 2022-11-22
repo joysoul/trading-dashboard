e55838972d2f77d55ba9b1dd5c6787e2ae9c6a88
joysoul <34410341+joysoul@users.noreply.github.com>
2022-11-22 15:27:52 +0800

    Update and rename api-keys.example.js to api-keys.js

--- src/config/api-keys.js
+++ src/config/api-keys.js
@@ -4,6 +4,12 @@ export const CONFIGURATION = {
     secret: 'BINANCE_API_SECRET',
     exchangeType: 'futures' // spot is not supported yet
   },
+  okx: {
+    key: 'OKX_API_KEY',
+    secret: 'OKX_API_SECRET',
+    alp:'YOUR WORD',
+    exchangeType: 'futures' // spot is not supported yet
+  },
   profile: {
     name: 'John Doe'
   }
